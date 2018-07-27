be230789ce7fa78982cb4fade96fd900765375ad
kamikura <>
2018-07-27 21:38:04 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all.order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def create
+    @post = Post.new(image: params[:image], text: params[:text])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.text = params[:text]
+    @post.image = params[:image]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    @post.save
+    redirect_to("/post/index")
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'posts#index'
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+end
