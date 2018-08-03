2f15e4275c8aa940325ef119f7e65caa6a5ed608
kamikura <>
2018-08-03 17:03:26 +0900

    add_user_authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:index, :show, :edit, :update]
+  before_action :forbid_login_user, only: [:new, :create, :login, :login_form]
+
   def index
   end
 
