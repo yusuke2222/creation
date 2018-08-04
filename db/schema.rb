cc7fe4d8fdab09cd939b7741588fbd6bba2e5915
kamikura <>
2018-08-04 10:18:41 +0900

    remove_posts_name_column

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180803160307) do
+ActiveRecord::Schema.define(version: 20180804010208) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "name",       limit: 255
     t.text     "text",       limit: 65535
     t.text     "image",      limit: 65535
     t.datetime "created_at"
