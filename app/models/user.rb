a0df67170631a0db009393ced79955e9fc8c1a9b
kamikura <>
2018-08-06 13:24:38 +0900

    user_restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   validates :name, {presence: true}
   validates :email, {uniqueness: true, presence: true}
   validates :password, {presence: true}
+
+  def posts
+    return Post.where(user_id: self.id)
+  end
 end
