2bb1069f68332cd6f18e152517bee5dc277e22c1
kamikura <>
2018-08-24 10:49:48 +0900

    edit_comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(text: params[:text], post_id: params[:post_id], user_id: @current_user.id)
+    @comment = Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: @current_user.id)
     redirect_to("/posts/#{@comment.post_id}")
   end
 
@@ -9,5 +9,10 @@ class CommentsController < ApplicationController
     @comment.destroy
     flash[:notice] ="コメントを削除しました"
     redirect_to("/posts/#{@comment.post_id}")
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :post_id)
   end
 end
