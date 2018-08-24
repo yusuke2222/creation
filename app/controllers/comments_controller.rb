ff000050762edf205f6160ff65d0aa1a7980c4b0
kamikura <>
2018-08-24 22:14:44 +0900

    create_asynchronous

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: @current_user.id)
-    redirect_to("/posts/#{@comment.post_id}")
+    respond_to do |format|
+      format.html { redirect_to post_path(params[:post_id]) }
+      format.json
+    end
+    # redirect_to("/posts/#{@comment.post_id}")
   end
 
   def destroy
